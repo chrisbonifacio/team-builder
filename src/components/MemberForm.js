15aebd9ef5e67d15193bc55e4d418e78b327f839
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-08-21 18:20:55 -0400

    styled form components

--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -1,5 +1,64 @@
 import React, { useState } from "react";
 
+import styled from "styled-components";
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  background: #fff;
+  margin: 2rem auto;
+  padding: 1rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  color: #4a4a4a;
+  margin-bottom: 1rem;
+`;
+
+const Field = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1.5rem;
+`;
+
+const Label = styled.label`
+  color: #4a4a4a;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-right: 0.8rem;
+`;
+
+const Input = styled.input`
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 2px;
+  padding: 1rem 0;
+`;
+
+const TextArea = styled.textarea`
+  display: flex;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 2px;
+`;
+
+const Button = styled.button`
+  color: #4a4a4a;
+  font-weight: 600;
+  padding: 0.5rem;
+  background: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 2px;
+`;
+
 const MemberForm = props => {
   // State
   const [member, setMember] = useState({
@@ -17,55 +76,70 @@ const MemberForm = props => {
 
     console.log(event.target.value);
   };
-  const randomMember = { ...member, ["name"]: "Chris" };
-  console.log(randomMember);
+
+  //   const randomMember = { ...member, ["name"]: "Chris" };
+  //   console.log(randomMember);
 
   const SubmitForm = event => {
     event.preventDefault();
     // created id property for use as a key, can be used to find the location of an item in a list
     const newMember = { ...member, id: Date.now() };
+    // takes an object as a argument and adds it to the memberList
     props.addMember(newMember);
   };
 
   return (
-    <form className="form" onSubmit={SubmitForm}>
-      <label htmlFor="name">Name: </label>
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        value={member.name}
-        onChange={changeHandler}
-      />
+    // When submitting the form, we want it to run the function that adds the new object to the list of members
+    <FormWrapper>
+      <Title>Team Builder</Title>
+      <Form className="form" onSubmit={SubmitForm}>
+        <Field>
+          <Label htmlFor="name">Name: </Label>
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={member.name}
+            onChange={changeHandler}
+          />
+        </Field>
 
-      <label htmlFor="email">Email: </label>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={member.email}
-        onChange={changeHandler}
-      />
+        <Field>
+          <Label htmlFor="email">Email: </Label>
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={member.email}
+            onChange={changeHandler}
+          />
+        </Field>
 
-      <label htmlFor="role">Role: </label>
-      <input
-        type="text"
-        name="role"
-        placeholder="Role"
-        value={member.role}
-        onChange={changeHandler}
-      />
+        <Field>
+          <Label htmlFor="role">Role: </Label>
+          <Input
+            type="text"
+            name="role"
+            placeholder="Role"
+            value={member.role}
+            onChange={changeHandler}
+          />
+        </Field>
 
-      <label htmlFor="bio">Bio: </label>
-      <textarea
-        type="text"
-        name="bio"
-        placeholder="Bio"
-        value={member.bio}
-        onChange={changeHandler}
-      />
-      <button type="submit">Add New Member</button>
-    </form>
+        <Field>
+          <Label htmlFor="bio">Bio: </Label>
+          <TextArea
+            type="text"
+            name="bio"
+            placeholder="Bio"
+            value={member.bio}
+            onChange={changeHandler}
+          />
+        </Field>
+
+        <Button type="submit">Add New Member</Button>
+      </Form>
+    </FormWrapper>
   );
 };
 
